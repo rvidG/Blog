70302754ccc989363eace248a978216000638f96
rvid <github@rvid.eu>
2025-08-14 01:22:19 +0200

    Bugs

--- build.sh
+++ build.sh
@@ -1,21 +1,36 @@
 #!/bin/bash
 
-<<<<<<< HEAD
-files=$($(find blog/ -name "*.md"))
-echo $files
-#marked
-=======
-if [ -z $1 ]; then
-  echo "Usage: $0 [-t <template.html>] <md-src> <build-dst>"
+function usage() {
+  echo "Usage: $0 [-t <template.html>] [-e <file extension>] <md-src> <build-dst>"
   echo "Options:"
-  echo "  -t template.html  Specify a template HTML file (where your navbar is etc)"
-  echo "  <md-src>          Directory of markdown files"
-  echo "  <build-dst>       Destination of built files"
+  echo "  -t template.html     Specify a template HTML file (where your navbar is etc)"
+  echo "  -e <file extension>  File extension/postfix to use"
+  echo "  -h                   Show this help message"
+  echo "  <md-src>             Directory of markdown files"
+  echo "  <build-dst>          Destination of built files"
   echo -e "\nExample:"
-  echo "  $0 -t template.php markdown/ blog/"
-fi
+  echo "  $0 -t template.php -e ".php" markdown/ blog/"
+  exit 0
+}
 
-files=$(ls "$1" | grep "\.md")
-echo $files
-marked
->>>>>>> 101d58e (Continue)
+while getopts 'teh' OPTION; do
+  case "$OPTION" in
+  t)
+    template="$OPTARG"
+    ;;
+  e)
+    ext="$OPTARG"
+    ;;
+  h)
+    usage
+    ;;
+  esac
+done
+
+if [ -z $2 ]; then
+  usage
+else
+  files=$(ls "$1" | grep "\.md")
+  echo $files
+  marked "$(cat $files)"
+fi
