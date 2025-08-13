bd260c18ff518deab9d7fac1cdb8cfe7df712c69
rvid <github@rvid.eu>
2025-08-14 00:22:51 +0200

    Continue

--- build.sh
+++ build.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+<<<<<<< HEAD
 files=$($(find blog/ -name "*.md"))
 echo $files
 #marked
+=======
+if [ -z $1 ]; then
+  echo "Usage: $0 [-t <template.html>] <md-src> <build-dst>"
+  echo "Options:"
+  echo "  -t template.html  Specify a template HTML file (where your navbar is etc)"
+  echo "  <md-src>          Directory of markdown files"
+  echo "  <build-dst>       Destination of built files"
+  echo -e "\nExample:"
+  echo "  $0 -t template.php markdown/ blog/"
+fi
+
+files=$(ls "$1" | grep "\.md")
+echo $files
+marked
+>>>>>>> 101d58e (Continue)
