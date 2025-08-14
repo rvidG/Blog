ce63308d5cd7dc173362bcc9109baa2b522620e7
rvid <github@rvid.eu>
2025-08-15 01:18:55 +0200

    Continue

--- build.sh
+++ build.sh
@@ -4,16 +4,18 @@ function usage() {
   echo "Usage: $0 [-t <template.html>] [-e <file extension>] <md-src> <build-dst>"
   echo "Options:"
   echo "  -t template.html     Specify a template HTML file (where your navbar is etc)"
-  echo "  -e <file extension>  File extension/postfix to use"
+  echo "  -e <file extension>  File extension/postfix to use (Default: .html)"
   echo "  -h                   Show this help message"
   echo "  <md-src>             Directory of markdown files"
   echo "  <build-dst>          Destination of built files"
   echo -e "\nExample:"
-  echo "  $0 -t template.php -e ".php" markdown/ blog/"
+  echo "  $0 -t template.html -e \".html\" markdown/ blog/"
   exit 0
 }
 
-while getopts 'teh' OPTION; do
+ext=".html"
+
+while getopts 't:e:h' OPTION; do
   case "$OPTION" in
   t)
     template="$OPTARG"
@@ -24,13 +26,34 @@ while getopts 'teh' OPTION; do
   h)
     usage
     ;;
+  ?)
+    usage
+    ;;
   esac
 done
 
-if [ -z $2 ]; then
+shift $((OPTIND - 1))
+
+if [ $# -ne 2 ]; then
   usage
-else
-  files=$(ls "$1" | grep "\.md")
-  echo $files
-  marked "$(cat $files)"
 fi
+
+md_src="$1"
+build_dst="$2"
+
+mkdir -p "$build_dst"
+
+for filepath in "$md_src"/*.md; do
+  [ -f "$filepath" ] || continue
+
+  export FILENAME=$(basename "$filepath" .md)
+  export CONTENT=$(marked "$filepath")
+
+  output_file="$build_dst/$FILENAME$ext"
+
+  if [ -n "$template" ]; then
+    envsubst <"$template" >"$output_file"
+  else
+    echo "$CONTENT" >"$output_file"
+  fi
+done
